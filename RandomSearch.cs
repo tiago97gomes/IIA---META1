1a6536164b3faf3f8fc0a13f7a737c4f06914e10
tiago97gomes <tiagofrend@hotmail.com>
2018-04-10 00:34:58 +0100

    novos scripts
    Sofrega: em processo
    Depth: check
    Limited Depth: check
    Progressive Depth: check
    Aleatorio : check

---
+++ RandomSearch.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSearch : SearchAlgorithm {
+
+	private List<SearchState> openQueue;
+
+
+	protected override void Begin () {
+		startNode = GridMap.instance.NodeFromWorldPoint (startPos);
+		targetNode = GridMap.instance.NodeFromWorldPoint (targetPos);
+		//estas duas linhas funcionam para quase ou todos os algoritmos
+
+		SearchState start = new SearchState (startNode, 0);
+		openQueue = new List<SearchState> ();
+		openQueue.Add(start);
+		
+	}
+	
+	protected override void Step () {
+		
+		if (openQueue.Count > 0)
+		{
+			int index = Random.Range (0, openQueue.Count);
+			SearchState currentState = openQueue[index];//tirar o nó da fila
+			Debug.Log(index);
+			openQueue.RemoveAt(index);
+			VisitNode (currentState);
+			if (currentState.node == targetNode) {
+				solution = currentState;
+				finished = true;
+				running = false;
+				foundPath = true;
+			} else {
+				foreach (Node suc in GetNodeSucessors(currentState.node)) {
+					SearchState new_node = new SearchState(suc, suc.gCost + currentState.g, currentState);
+					openQueue.Add (new_node);
+				}
+				// for energy
+				if ((ulong) openQueue.Count > maxListSize) {
+					maxListSize = (ulong) openQueue.Count;
+				}
+			}
+		}
+		else
+		{
+			finished = true;
+			running = false;
+			foundPath = true;
+		}
+
+	}
+}
---
+++ SofregaSearch.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SofregaSearch : SearchAlgorithm {
+
+	private List<SearchState> SortedList;
+
+
+	protected override void Begin () {
+		startNode = GridMap.instance.NodeFromWorldPoint (startPos);
+		targetNode = GridMap.instance.NodeFromWorldPoint (targetPos);
+		//estas duas linhas funcionam para quase ou todos os algoritmos
+
+		SearchState start = new SearchState (startNode, 0);
+		SortedList = new List<SearchState> ();
+		SortedList.Add(start);
+		
+	}
+	
+	protected override void Step () {
+		
+		if (SortedList.Count > 0)
+		{
+			SearchState currentState = SortedList[0];
+			SortedList.RemoveAt(0);
+			VisitNode (currentState);
+			if (currentState.node == targetNode) {
+				solution = currentState;
+				finished = true;
+				running = false;
+				foundPath = true;
+			} else {
+				foreach (Node suc in GetNodeSucessors(currentState.node)) {
+					SearchState new_node = new SearchState(suc, suc.gCost + currentState.g, currentState);
+
+					InsertionSort (new_node);
+				}
+				if ((ulong) SortedList.Count > maxListSize) {
+					maxListSize = (ulong) SortedList.Count;
+				}
+			}
+		}
+		else
+		{
+			finished = true;
+			running = false;
+			foundPath = true;
+		}
+
+	}
+
+	void InsertionSort(SearchState node){
+		for (int i = 0; i < SortedList.Count; i++) {
+			if (SortedList [i].g > node.g) {
+				SortedList.Insert (i, node);
+				return;
+			}
+		}
+	}
+}
+
