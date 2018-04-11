15c2277ebabc33de43f3cb980d5b2b4d0f86cfb6
Linhares <rlinhares@student.dei.uc.pt>
2018-04-12 00:38:41 +0100

    a* e esfrega

---
+++ PesquisaSôfrega.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PesquisaSôfrega : SearchAlgorithm {
+
+	private PriorityQueue openQueue;
+
+	protected override void Begin () {
+		startNode = GridMap.instance.NodeFromWorldPoint (startPos);
+		targetNode = GridMap.instance.NodeFromWorldPoint (targetPos);
+
+		SearchState start = new SearchState (startNode, 0);
+		openQueue = new PriorityQueue ();
+		openQueue.Add(start, 0);
+
+	}
+
+	protected override void Step () {
+
+		if (openQueue.Count > 0)
+		{
+			SearchState currentState = openQueue.PopFirst();
+			VisitNode (currentState);
+			if (currentState.node == targetNode) {
+				solution = currentState;
+				finished = true;
+				running = false;
+				foundPath = true;
+			} else {
+				foreach (Node suc in GetNodeSucessors(currentState.node)) {
+					SearchState new_node = new SearchState(suc, suc.gCost + currentState.g, GetHeuristic(currentState),currentState);
+					openQueue.Add (new_node, (int)new_node.h);
+				}
+				// for force
+				if ((ulong) openQueue.Count > maxListSize) {
+					maxListSize = (ulong) openQueue.Count;
+				}
+			}
+		}
+		else
+		{
+			finished = true;
+			running = false;
+		}
+	}
+}
