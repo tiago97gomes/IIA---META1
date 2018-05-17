eab366cd49f626b789a566cc4533fcbb0a789a10
Linhares <rlinhares@student.dei.uc.pt>
2018-05-17 02:24:08 +0100

    meta3init

---
+++ Meta3/GeneticAlgorithm.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneticAlgorithm : MetaHeuristic {
+	public float mutationProbability;
+	public float crossoverProbability;
+	public int tournamentSize;
+	public bool elitist;
+
+	public override void InitPopulation () {
+		population = new List<Individual> ();
+		// jncor 
+		while (population.Count < populationSize) {
+			GeneticIndividual new_ind = new GeneticIndividual (topology);
+			new_ind.Initialize ();
+			population.Add (new_ind);
+		}
+	}
+
+	//The Step function assumes that the fitness values of all the individuals in the population have been calculated.
+	public override void Step() {
+		List<Individual> new_pop = new List<Individual> ();
+
+		updateReport (); //called to get some stats
+		// fills the rest with mutations of the best !
+		for (int i = 0; i < populationSize ; i++) {
+			GeneticIndividual tmp = (GeneticIndividual) overallBest.Clone ();
+			tmp.Mutate (mutationProbability);
+			new_pop.Add (tmp.Clone());
+		}
+
+		population = new_pop;
+
+		generation++;
+	}
+}
---
+++ Meta3/GeneticIndividual.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneticIndividual : Individual {
+
+
+	GeneticIndividual(int[] topology) : base(topology) {
+	}
+
+	public override void Initialize () 
+	{
+		for (int i = 0; i < totalSize; i++) {
+			genotype [i] = Random.Range (-1.0f, 1.0f);
+		}
+	}
+		
+	public override void Crossover (Individual partner, float probability, int termos)
+	{
+		int rand;
+		if (Random.Range (0.0f, 1.0f) < probability) {
+			List<int> cortes = new List<int>();
+			for (int i = 0; i < termos; i++) {
+				while (cortes.Contains(rand = Random.Range(0, totalSize)));
+
+				cortes.Add (rand);
+			}
+			cortes.Sort();
+			bool v = true;
+			for (int i = 0; i < termos; i++) {
+				if (v) {
+					if (i - 1 == termos)
+						partner.getGenotype.CopyTo (genotype, termos [i]);
+					partner.getGenotype.CopyTo (genotype, termos [i], termos [i + 1]);
+				}
+				v = v + false;
+			}
+		}
+	}
+
+	public override void Mutate (float probability)
+	{
+		for (int i = 0; i < totalSize; i++) {
+			if (Random.Range (0.0f, 1.0f) < probability) {
+				genotype [i] = Random.Range (-1.0f, 1.0f);
+			}
+		}
+	}
+
+	public override Individual Clone ()
+	{
+		GeneticIndividual new_ind = new GeneticIndividual(this.topology);
+
+		genotype.CopyTo (new_ind.genotype, 0);
+		new_ind.fitness = this.Fitness;
+		new_ind.evaluated = false;
+
+		return new_ind;
+	}
+
+}
